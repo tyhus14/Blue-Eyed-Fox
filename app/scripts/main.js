040643cc997e18c4034e31e7199ad169b497be18
tyhus14 <huston.tyler3@gmail.com>
2014-11-25 10:42:49 -0500

    added webapp page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,89 @@
-var target = $("#scrolled-to").offset().top,
-    timeout = null;
+$(function() {
+    
 
-$(window).scroll(function () {
-    if (!timeout) {
-        timeout = setTimeout(function () {
-            console.log('scroll');            
-            clearTimeout(timeout);
-            timeout = null;
-            if ($(window).scrollTop() >= target) {
-                $(".header").addClass('appear');
-            }
-            if ($(window).scrollTop() + 400 <= target) {
-                $(".header").removeClass('appear');
-            }
-        }, 250);
-    }
+
+    var target = $("#scrolled-to").offset().top,
+        timeout = null;
+
+    $(window).scroll(function () {
+        if (!timeout) {
+            timeout = setTimeout(function () {
+                console.log('scroll');            
+                clearTimeout(timeout);
+                timeout = null;
+                if ($(window).scrollTop() >= target) {
+                    $(".header").addClass('appear');
+                }
+                if ($(window).scrollTop() + 400 <= target) {
+                    $(".header").removeClass('appear');
+                }
+            }, 250);
+        }
+    });
+
+
+    $('.featured-one').hover(function(){
+        $('.featured-hover-one').toggleClass('active');
+        $('.featured-text-one').toggleClass('active');
+    });
+
+    $('.featured-two').hover(function(){
+        $('.featured-hover-two').toggleClass('active');
+        $('.featured-text-two').toggleClass('active');
+    });
+
+    $('.featured-three').hover(function(){
+        $('.featured-hover-three').toggleClass('active');
+        $('.featured-text-three').toggleClass('active');
+    });
+
+    $('.featured-four').hover(function(){
+        $('.featured-hover-four').toggleClass('active');
+        $('.featured-text-four').toggleClass('active');
+    });
+
+    $('.featured-five').hover(function(){
+        $('.featured-hover-five').toggleClass('active');
+        $('.featured-text-five').toggleClass('active');
+    });
+
+    $('.featured-six').hover(function(){
+        $('.featured-hover-six').toggleClass('active');
+        $('.featured-text-six').toggleClass('active');
+    });
+
+    $('.logo').hover(function(){
+        $('.bubble').toggleClass('active');
+    });
+
+    $('.menu-toggle').click(function(){
+        $('.menu-nav').addClass('active');
+        $('body').addClass('active');
+        $('.logo').addClass('nav-active');
+        $(this).addClass('active');
+        $('.bubble').addClass('nav-up');
+    });
+
+    $('.menu-toggle-nav').click(function(){
+        $('.menu-nav').removeClass('active');
+        $('body').removeClass('active');
+        $('.logo').removeClass('nav-active');
+        $('.menu-toggle').removeClass('active');
+        $('.bubble').removeClass('nav-up');
+    });
+
+
+
+    $(".nav-item").bind("click", function(e) {
+
+        e.preventDefault();
+        var location = $(this).attr("href")
+
+        $('html, body').animate({
+            scrollTop: $(location).offset().top - 60
+        }, 1500);
+
+    });
+
+
 });
